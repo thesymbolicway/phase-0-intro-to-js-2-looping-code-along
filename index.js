54f18d49e942e124ca0103af6dd7916075536605
daniel.arisa@yahoo.com <daaniel.arisa@yahoo.com>
2022-07-21 21:42:35 -0500

    commit tree 80248eada953be8309f155789b9146096e680d0e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+// const gifts = ["teddy bear", "drone", "doll"]
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//       console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//       debugger;
+//     }
+  
+//     return gifts;
+//   }
+  
+//   wrapGifts(gifts);
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names, event) {
+    return  names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+ }
+ 
+ console.log(writeCards(names, "surprise"))
+
+ function countDown (){
+    let countDown = 0;
+while (countDown < 11) {
+  console.log(countDown++);
+}
+ }
